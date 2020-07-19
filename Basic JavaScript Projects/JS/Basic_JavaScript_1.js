e6a31dc08c16e39834702534db490b575de7e8e8
Admin <spirit2914@outlook.com>
2020-07-18 21:09:06 -0500

    Created Basic_JavaScript_Project

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,24 @@
+
+    
+        //window.alert("Welcome to the Tech Academy");
+		document.writeln("Hello, friends!<br>");
+
+        alert("Hello,World!");
+        var A="This is a string";
+        document.writeln(A);
+        document.writeln("<br>");
+
+        document.writeln("Lisa said to Christ, \"Hello, nice to meet you\". Christ said, \"Hello\" to her <br>");
+
+        document.writeln("\"Be who you are and say what you feel " +"because those who mind don\'t matter and who matter don\'t mind\""+ "-Dr. Seuss<br>");
+
+        var family="The Geller", son="Ross Geller", daughter="Monica Geller";
+        document.writeln(son+"<br>"); 
+        document.writeln(family+"<br>");
+
+        var blue="I have a candy.";
+        var blue=blue.fontcolor("blue");
+        document.writeln(blue+"<br>");
+
+        var result = 3+3; //Defining a result variable and assigning the math of addition to result
+        document.writeln(result);//Print result on screen
