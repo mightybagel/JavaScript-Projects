9915eb1a5787f9f2d201f7aa46b47a704afb7ebc
Admin <spirit2914@outlook.com>
2020-07-20 13:22:35 -0500

    Updated the Basic_HTML_6.html
    Added the voting check

--- Basic JavaScript Projects/JS/Basic_JavaScript_6.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_6.js
@@ -1,6 +1,16 @@
-function RideFunction(){
-    var height, can_Ride;
-    height = document.getElementById("Height").innerHTML.value;
-    can_Ride = (height < 52)?"You are too short":"You are tall enough";
-    document.getElementById("Ride").innerHTML= can_Ride + " to ride";
-};
+/*================================================
+    Check the availability to ride using height
+================================================*/
+/*function Compare(){
+    var height, canRide;
+    height=document.getElementById("Height").value;
+    canRide=(height<52)?"You are not tall enough":"You are tall enough";
+    document.getElementById("Rider").innerHTML=canRide +" to ride";
+}*/
+function Check_age(){
+    var age, canVote;
+    age=document.getElementById("Qualify_to_Vote").value;
+    canVote=(age<18)?"You are not old enough to vote":"You can vote";
+    document.getElementById("Eligibility").innerHTML=canVote
+
+}
