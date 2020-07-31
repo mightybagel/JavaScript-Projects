47ff92c045d627e54765d376d18b44cc585cfc4d
Admin <spirit2914@outlook.com>
2020-07-30 19:42:02 -0500

    Fixed file 7

--- Basic JavaScript Projects/JS/Basic_JavaScript_7.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_7.js
@@ -1,10 +1,33 @@
-function myFunction() {
-    var hour = new Date().getHours(); 
-    var greeting;
-    if (hour < 18) {
-      greeting = "Good day";
-    } else {
-      greeting = "Good evening";
-    }
-    document.getElementById("demo").innerHTML = greeting;
-  };
+var num = 10;
+function check_season() {
+  var d= new Date();
+  var date = parseInt(d.getDate());
+  var month= parseInt(d.getMonth() + 1);
+  console.log(d);
+  console.log(date);
+  console.log(month);
+  if (month>=3 && month<=5 && date>=1 && date<=31)
+    return "We are in spring";
+  else if (month>=6 && month<=8 && date>=1 && date<=31)
+    return "We are in summer";
+  else if (month>=9 && month<=11 && date>=1 && date<=30)
+    return "We are in autumn";
+  else
+    return "We are in winter";
+}
+function check_time(){
+  var time = new Date().getHours();
+  console.log(time);
+  if (time <12 && time >0 )
+    return "It is morning time";
+  else
+    return "It is afternoon time";
+}
+
+function myFunction(){
+  // use local variable in  the different function
+  //d.Date();
+  //console.log(d);
+  document.getElementById("demo_1").innerHTML=check_season();
+  document.getElementById("demo_2").innerHTML=check_time();
+}
